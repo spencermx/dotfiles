029fec8b0879e51b95d77a47fd4c0c46209eeaf8
spencermx <138399420+spencermx@users.noreply.github.com>
2026-09-16 21:55:42 -0500

    hyprland: start mako with waybar

--- archlinux/config/hypr/hyprland.lua
+++ archlinux/config/hypr/hyprland.lua
@@ -64,7 +64,7 @@ local menu        = "bemenu-run"
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 hl.on("hyprland.start", function()
-    hl.exec_cmd("waybar & hyprpaper")
+    hl.exec_cmd("waybar & hyprpaper & mako")
 end)
 
 
